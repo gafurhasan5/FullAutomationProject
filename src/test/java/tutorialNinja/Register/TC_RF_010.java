2ee679da50cfa3029cbe0a55fb657dc36ef0234c
gafurhasan5 <gafurhasan5@gmail.com>
2026-01-27 15:43:20 +0530

    updated TC_RF_010 to verify register page navigation with diff type invalid email

--- src/test/java/tutorialNinja/Register/TC_RF_010.java
+++ src/test/java/tutorialNinja/Register/TC_RF_010.java
@@ -27,7 +27,6 @@ public class TC_RF_010 {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 		driver.manage().window().maximize();
 		driver.get("https://tutorialsninja.com/demo/");
-
 		driver.findElement(By.xpath("//span[text()='My Account']")).click();
 		driver.findElement(By.linkText("Register")).click();
 
@@ -42,22 +41,56 @@ public class TC_RF_010 {
 		driver.findElement(By.name("agree")).click();
 		driver.findElement(By.xpath("//input[@value='Continue']")).click();
 		Thread.sleep(3000);
-		File screenShot = driver.findElement(By.xpath("//form[@class='form-horizontal']"))
+		File screenShot1 = driver.findElement(By.xpath("//form[@class='form-horizontal']"))
+				.getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(screenShot1,
+				new File("C:\\FullAutomationLiveProject\\FullAutomationProject\\Screenshots\\sc1Actual.png"));
+		Assert.assertFalse(CompareTwoScreenshots(
+				"C:\\FullAutomationLiveProject\\FullAutomationProject\\Screenshots\\sc1Actual.png",
+				"C:\\FullAutomationLiveProject\\FullAutomationProject\\\\Screenshots\\Sc1Expected.png"));
+		Thread.sleep(3000);
+
+		driver.findElement(By.id("input-email")).clear();
+		driver.findElement(By.id("input-email")).sendKeys("rehankhan@");
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+		Thread.sleep(3000);
+		File screenShot2 = driver.findElement(By.xpath("//form[@class='form-horizontal']"))
 				.getScreenshotAs(OutputType.FILE);
 
-		FileHandler.copy(screenShot,
-				new File("C:\\FullAutomationLiveProject\\FullAutomationProject\\Screenshots\\sc1Actual.png"));
-		BufferedImage actualbufferedImag = ImageIO.read(
-				new File("C:\\\\FullAutomationLiveProject\\\\FullAutomationProject\\\\Screenshots\\\\sc1Actual.png"));
-		BufferedImage expectedbufferedImag = ImageIO
-				.read(new File("C:\\FullAutomationLiveProject\\FullAutomationProject\\Screenshots\\Sc1Expected.png"));
+		FileHandler.copy(screenShot2,
+				new File("C:\\FullAutomationLiveProject\\FullAutomationProject\\Screenshots\\sc2Actual.png"));
+
+		Assert.assertFalse(CompareTwoScreenshots(
+				"C:\\FullAutomationLiveProject\\FullAutomationProject\\Screenshots\\sc2Actual.png",
+				"C:\\FullAutomationLiveProject\\FullAutomationProject\\Screenshots\\Sc2Expected.png"));
+		Thread.sleep(3000);
+
+		driver.findElement(By.id("input-email")).clear();
+		driver.findElement(By.id("input-email")).sendKeys("rehankhangmail");
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+		Thread.sleep(3000);
+		File screenShot3 = driver.findElement(By.xpath("//form[@class='form-horizontal']"))
+				.getScreenshotAs(OutputType.FILE);
+
+		FileHandler.copy(screenShot3,
+				new File("C:\\FullAutomationLiveProject\\FullAutomationProject\\Screenshots\\sc3Actual.png"));
+
+		Assert.assertFalse(CompareTwoScreenshots(
+				"C:\\FullAutomationLiveProject\\FullAutomationProject\\Screenshots\\sc3Actual.png",
+				"C:\\FullAutomationLiveProject\\FullAutomationProject\\Screenshots\\Sc3Expected.png"));
+
+		driver.quit();
+	}
+
+	public boolean CompareTwoScreenshots(String actualImagePath, String expectedImagePath) throws IOException {
+		BufferedImage actualbufferedImag = ImageIO.read(new File(actualImagePath));
+		BufferedImage expectedbufferedImag = ImageIO.read(new File(expectedImagePath));
 		ImageDiffer imgdiffer = new ImageDiffer();
 		ImageDiff ImageDifferences = imgdiffer.makeDiff(expectedbufferedImag, actualbufferedImag);
 		// If image has no difference it return true
 		boolean b = ImageDifferences.hasDiff();
 		System.out.println(b);
-		Assert.assertFalse(ImageDifferences.hasDiff());
-		driver.quit();
+		return ImageDifferences.hasDiff();
 	}
 
 }
